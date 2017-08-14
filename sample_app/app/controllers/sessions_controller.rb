d136723a29eb691e73d4a9df64432c573706bae1
Mao Asada <mao.asada@livesense.co.jp>
2017-08-14 19:13:41 +0900

    &.を用いた書き方に変更

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       should_remember_user? ? remember(user) : forget(user)
       redirect_to user
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
     end
+
   end
 
   def destroy
