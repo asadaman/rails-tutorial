825fad5510ded809a4bd79a063e6fa6a282096d2
Mao Asada <mao.asada@livesense.co.jp>
2017-08-14 18:50:08 +0900

    should_remember_user?をbooleanを返すように修正

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def should_remember_user?
-    params[:session][:remember_me]
+    checkbox_paramater = params[:session][:remember_me]
+    checkbox_paramater == '1'? true : false
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      should_remember_user? == '1'? remember(user) : forget(user)
+      should_remember_user? ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
