dd32356d3c028dac09764c9d111eb0c6fa7ca999
Mao Asada <mao.asada@livesense.co.jp>
2017-08-17 11:41:51 +0900

    ユーザーの一覧ページを追加

--- sample_app/test/integration/users_edit_test.rb
+++ sample_app/test/integration/users_edit_test.rb
@@ -20,8 +20,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "succcessful edit with friendly forwarding" do
     get edit_user_path(@user)
+    assert session[:forwarding_url]
     log_in_as(@user)
-    assert_redirected_to edit_user_url(@user)
+    assert_redirected_to edit_user_url(@user) || default
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
