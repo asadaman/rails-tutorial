fdd2f50969595fe3a6f4b6209204ce727b7f74ff
Mao Asada <mao.asada@livesense.co.jp>
2017-08-22 16:22:59 +0900

    update_attributeの呼び出しをupdate_columns呼び出しにまとめる

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -29,8 +29,7 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_attribute(:actevated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
