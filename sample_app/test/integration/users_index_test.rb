b154b49658c5a4c7ef775115b526a4e8de1922c4
Mao Asada <mao.asada@livesense.co.jp>
2017-08-22 18:14:06 +0900

    /usersに対する統合テストを修正

--- sample_app/test/integration/users_index_test.rb
+++ sample_app/test/integration/users_index_test.rb
@@ -9,9 +9,21 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get users_path
     assert_template 'users/index'
+    activated_users ||= User.where(activated: true)
+    test_for_more_than_30_activated_users if activated_users.size > 30
+    # assert_select 'div.pagination' if users.size > 30
+    # User.paginate(page: 1).each do |user|
+    #   assert_select 'a[href=?]', user_path(user), text: user.name
+    # end
+  end
+
+  private
+
+  def test_for_more_than_30_activated_users
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
+
 end
