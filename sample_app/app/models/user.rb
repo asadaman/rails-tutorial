73bdb1922d49d2b482c66b88f4eb80204120aa3f
Mao Asada <mao.asada@livesense.co.jp>
2017-08-24 13:36:37 +0900

    パスワード再設定用メールのテストを追加

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   end
 
   def create_reset_digest
-    self.reset_token = User.new_token
+    self.reset_token = Authentication.new_token
     update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
