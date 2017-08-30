46683ca56c0bb1de2ac2ef300fb33282979f1c1e
Mao Asada <mao.asada@livesense.co.jp>
2017-08-30 17:35:32 +0900

    Micropostを追加、テストを追加

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -34,9 +35,7 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = Authentication.new_token
-    # update_attribute(:reset_digest, Authentication.convert_string_to_digest(reset_token))
-    # update_attribute(:reset_sent_at, Time.zone.now)
-      update_columns(reset_digest: Authentication.convert_string_to_digest(reset_token), reset_sent_at: Time.zone.now)
+    update_columns(reset_digest: Authentication.convert_string_to_digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   def password_reset_expired?
